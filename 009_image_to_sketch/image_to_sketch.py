6652271b3789ee56d74105ac1419100564b5b529
Sajjad Aemmi <sajjadaemmi@gmail.com>
2023-03-21 15:30:48 +0330

    image_to_sketch

---
+++ 009_image_to_sketch/image_to_sketch.py
@@ -0,0 +1,12 @@
+import cv2
+
+
+image = cv2.imread("sajjad.jpg")
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+image_gray_inverted = 255 - image_gray
+image_gray_inverted_blurred = cv2.GaussianBlur(image_gray_inverted, (21, 21), sigmaX=0, sigmaY=0)
+image_gray_blurred = 255 - image_gray_inverted_blurred
+image_sketch = (image_gray / image_gray_blurred) * 255
+
+cv2.imwrite("result.jpg", image_sketch)
