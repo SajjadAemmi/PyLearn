1d839e39646ef71cfdb44a2168eabf7aae306910
Sajjad Aemmi <sajjadaemmi@gmail.com>
2023-06-11 20:31:53 +0330

    Delete 017_combinations directory

--- 017_combinations/combinations_fruits.py
+++
@@ -1,9 +0,0 @@
-from itertools import combinations
-
-
-fruits = ['🍎', '🍊', '🍌', '🍒', '🍆', '🥕', '🥝', '🍍']
-number = 3
-
-for item in combinations(fruits, number):
-    temp = " ".join(word for word in item)
-    print(temp)
