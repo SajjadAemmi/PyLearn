1accca0977223dbee4706119dc520921b5842aa9
Sajjad Aemmi <sajjadaemmi@gmail.com>
2023-06-11 20:30:32 +0330

    Delete 005_youtube_downloader directory

--- 005_youtube_downloader/youtube_downloader.py
+++
@@ -1,5 +0,0 @@
-import pytube
-
-link = 'https://www.youtube.com/watch?v=lVFNRrL79w0'
-first_stream = pytube.YouTube(link).streams.first()
-first_stream.download(output_path='./', filename='test.mp4')
