e74987ba7f052a7d48e7db90478478d884da0831
Sajjad Aemmi <sajjadaemmi@gmail.com>
2023-07-26 14:51:05 +0330

    🐛 Fix bugs

--- 6.MachineLearning/6.5.LLS/lls.py
+++ 6.MachineLearning/6.5.LLS/lls.py
@@ -7,20 +7,15 @@ class LinearLeastSquare:
     
     def fit(self, X_train, Y_train):
         # train
-        X = X_train.reshape(-1, 1)
-        Y = Y_train.reshape(-1, 1)
         # w = شیب خط
         # w = (X.T * X)^-1 * X.T * Y
-        self.w = np.linalg.inv(X.T @ X) @ X.T @ Y
+        self.w = np.linalg.inv(X_train.T @ X_train) @ X_train.T @ Y_train
     
     def predict(self, X_test):
-        Y_pred = X_test * self.w
+        Y_pred = X_test @ self.w
         return Y_pred
     
     def evaluate(self, X_test, Y_test, loss="MAE"):
-        X_test = X_test.reshape(-1, 1)
-        Y_test = Y_test.reshape(-1, 1)
-        
         Y_pred = self.predict(X_test)
         Error = Y_test - Y_pred
 
